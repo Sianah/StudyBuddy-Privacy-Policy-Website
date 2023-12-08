cda34dbca546e4ea1c3619fa4bc95001b8314675
Sisi Small <smallsianah@gmail.com>
2023-12-07 21:02:06 -0500

    studybuddy deployment

---
+++ privacypolicy/src/App.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  // State declarations
+
+  return (
+    <div className="App">
+      
+      <h2>StudyBuddy Privacy Policy</h2>
+      
+      <div className="privacy-policy-container">
+
+
+      <p>Effective Date: 12/08/2023</p>
+
+      <p>Welcome to StudyBuddy! This Privacy Policy is designed to help you understand how we collect, use, and safeguard your personal information. By using StudyBuddy, you agree to the terms outlined in this Privacy Policy.</p>
+
+      Information We Collect:
+      <p>Account Information: To provide our services, we collect and store information you provide when you register for an account, including your name, email address, and profile picture.
+      Class Information: We gather information about your class or course to connect you with other users in similar classes for a more personalized experience.
+      Chat Data: We collect and store messages exchanged between users to facilitate communication within the app.</p>
+
+      How We Use Your Information:
+      <p>Connecting Users: We use class information to connect you with other users in the same class, enhancing your app experience.
+      Communication: Your email address is used for account-related communications, and we use chat data to facilitate conversations among users.
+      Improving Services: We may use aggregated and anonymized data to analyze usage patterns and improve our app's functionality.</p>
+
+      Sharing of Information:
+      <p>With Other Users: Your name, profile picture, and class information are visible to other users to facilitate connections and communication.
+      Third-Party Services: We may use third-party services for analytics and support, and your information may be shared with these services to improve our app.</p>
+
+      Data Security:
+      <p>We implement industry-standard security measures to protect your information from unauthorized access, disclosure, or alteration.
+      However, no method of transmission over the internet or electronic storage is 100% secure, so we cannot guarantee absolute security.</p>
+
+      Your Choices:
+      <p>You can edit your profile information at any time.
+      You can control who can contact you through app settings.</p>
+
+      Children's Privacy:
+      <p>StudyBuddy is intended for users aged 13 and older. We do not knowingly collect personal information from children under 13. If you believe we have collected information from a child under 13, please contact us.</p>
+
+      Updates to Privacy Policy:
+      <p>We may update this Privacy Policy to reflect changes in our practices or for other operational, legal, or regulatory reasons. We will notify you of any material changes through the app or other means.</p>
+
+      Contact Us:
+      <p>If you have any questions or concerns about our Privacy Policy, please contact us at ascarlato@stetson.edu.</p>
+
+      By using StudyBuddy, you agree to the terms outlined in this Privacy Policy.
+      StudyBuddy is committed to protecting your privacy and providing a secure and enjoyable experience.
+
+      <p>Thank you for using StudyBuddy!</p>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
